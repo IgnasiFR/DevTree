d1966bd6292f74f20d22cd7d338f9bab8c57d184
ClaireOnAir <andraia1994@gmail.com>
2024-12-09 23:34:12 +0100

    Creando servidor express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hola mundo desde DevTree");
+import express from "express";
+
+const app = express();
+
+//Routing
+app.get("/", (req, res) => {
+  res.send("Hola Mundo en Express");
+});
+
+//Establecemos el puerto para el arranque
+const port = process.env.PORT || 4000; // utilizamos la variable de entorno llamada puerto  o 4000 ( es para de cara el deploy al proveedor del host)
+
+app.listen(port, () => {
+  console.log("Servidor funcionando en el puerto:", port);
+});
