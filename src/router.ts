7836bd77f536bae40add28a28d9d96e289932e6c
IgnasiFR <ignasifr@uoc.edu>
2024-12-11 23:39:21 +0100

    He creado handlers para separar el código del router, añadido manejo de carga infinita al enviar o crear un registro

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,9 @@
 import { Router } from "express";
-import User from "./models/User";
+import { createAccount } from "./handlers";
 
 const router = Router()
 
-//Routing
-router.get("/", (req, res) => {
-    res.send("Hola Mundo en Express / TypeScript");
-  });
-
 // Authentication y registro
-router.post("/auth/register", async (req,res) => {
-    await User.create(req.body)
-})
+router.post("/auth/register", createAccount)
 
 export default router       
