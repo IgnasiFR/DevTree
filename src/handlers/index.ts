6876e254e546e844d60f935fa2ea07a4c532a4bb
IgnasiFR <ignasifr@uoc.edu>
2024-12-12 12:37:48 +0100

    Manejo de errores añadido, passwords sistema hash de passwords añadido

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,21 @@
-import type { Request, Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 
-export const createAccount = async (req: Request, res: Response) => {
+export const createAccount = async (req, res) => {
+
+    const {email, password} = req.body
+    const userExists = await User.findOne({email})
+    if (userExists) {
+        const error = new Error('El usuario ya está registrado')
+        return res.status(409).json({error: error.message}) // status() lo que hace es pasarle el error al cliente como respuesta
+
+    }
+
     const user = new User(req.body)
+    user.password = await hashPassword(password)
+    
     await user.save()
-    res.send('Registro Creado Correctamente')
+    res.status(201).send('Registro Creado Correctamente')
 }
 
