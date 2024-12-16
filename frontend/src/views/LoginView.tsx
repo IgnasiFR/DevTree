809c1a1476fef9446c90aaa0294e8dc076388ce2
IgnasiFR <ignasifr@uoc.edu>
2024-12-16 22:45:45 +0100

    primeros componentes y layouts

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom'
 export default function LoginView() {
   return (
     <>
-        <div className="text-2xl">LoginView</div>
-
-        <nav>
-            <Link to ="/auth/register">
+        <h1 className='text-4xl text-white font-bold'>Inicia Sesión</h1>
+        <nav className='mt-10'>
+            <Link className='text-center text-white text-lg block' 
+            to ="/auth/register">
                 ¿No tienes cuenta? Crea una aquí
             </Link>
         </nav>
